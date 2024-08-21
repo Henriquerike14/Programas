9f2262cf4ef93b83d96960978023d00dc7823f77
Henrique Gustavo <nylandrike@gmail.com>
2024-08-21 14:51:27 -0300

    Primeira versão

---
+++ Aula 1/Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner abacaxi = new Scanner(System.in);
+
+        System.out.println("Eu gosto de abacaxi");
+    }
+}
